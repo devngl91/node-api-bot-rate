61ef61327a4af207ac4ed1396661d63b0a54c442
devngl91 <nzl91@pm.me>
2023-11-18 18:15:48 -0300

    update .env/Dockerfile/install.sh

--- install_docker_ec2.sh
+++ install_docker_ec2.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
-# install of environment
+# SSM user didn't start in the home directory, so go there
 
 cd
-sudo yum update -y
-sudo yum install docker git npm -y
-sleep 2
-sudo npm install nodemon
-sleep 2
+
+sudo apt update -y
+
+sudo ufw allow 80
+
+sudo ufw allow 443
+
+sudo ufw allow 3000
+
+sudo ufw default deny incoming
+
+sudo ufw default allow outgoing
+
+sudo ufw enable -y
+
+sudo apt install docker.io git -y
+
+sudo apt install nodejs
+
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sleep 2
+
 sudo chmod +x /usr/local/bin/docker-compose
-sleep 2
+
 sudo service docker start
+
 sudo chmod 666 /var/run/docker.sock
-sleep 2
+
 git clone https://github.com/devngl91/node-api-bot-rate-limit-9m8dya9osydn897abgsdnasdadasdas.git
+
 cd node-api-bot-rate-limit-9m8dya9osydn897abgsdnasdadasdas
-sudo chmod -R 755 .
+
+npm install
+
+sleep 2
+
 docker-compose up -d
